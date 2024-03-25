8d04ca2b4c06da96a7d5f47fb8fc0d6f297dea3b
1slas <536099116@qq.com>
2024-03-25 17:55:47 +0800

    xxxxxx

---
+++ src/main/java/generator/mapper/ActivityMapper.java
@@ -0,0 +1,17 @@
+package generator.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import generator.domain.Activity;
+
+
+/**
+* @author 53609
+* @description 针对表【activity(社团活动表)】的数据库操作Mapper
+* @createDate 2024-03-25 17:34:54
+* @Entity generator.domain.Activity
+*/
+public interface ActivityMapper extends BaseMapper<Activity> {
+
+
+
+}
